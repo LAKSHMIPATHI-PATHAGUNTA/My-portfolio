bc70628a1e89afc25f5513e4752334a304170baa
P LAKSHMIPATHI <112970688+LAKSHMIPATHI-PATHAGUNTA@users.noreply.github.com>
2025-01-21 19:47:13 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,7 @@ const Skills = () => (
   <div className="resume-section">
     <h4>Skills</h4>
     <ul>
-      <li>Programming Languages: JavaScript, Python, C++,R,java
+      <li>Programming Languages: JavaScript, Python, C++,R,java<li>
       <li>Data Structures & Algorithms,SQL</li>
       <li>Development Tools: Git, VS Code</li>
     </ul>
