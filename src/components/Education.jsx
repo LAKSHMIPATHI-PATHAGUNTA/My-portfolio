c28636308711dd28d85e5f94bd2f521920e376e2
P LAKSHMIPATHI <112970688+LAKSHMIPATHI-PATHAGUNTA@users.noreply.github.com>
2025-01-21 19:34:16 +0530

    Update Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -4,7 +4,7 @@ const Education = () => (
   <div className="resume-section">
     <h4>Education</h4>
     <ul>
-      <li>B.Tech, SRM University AP, CGPA: 7.81 (Current), 2021-Present</li>
+      <li>B.Tech, SRM University AP, CGPA: 7.9 (Current), 2021-Present</li>
       <li>Senior Secondary, BIEAP Board, 96.5%, 2019-21</li>
       <li>Secondary, BSEAP Board, 95%, 2018-19</li>
     </ul>
